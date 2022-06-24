4a232496e35100317a27bd553ee84069fd2a916a
dekhil008 <dekhil.008@gmail.com>
2022-06-24 14:24:21 +0300

    update 2

--- scripts/projects.js
+++ scripts/projects.js
@@ -2,14 +2,20 @@ let PROJECTS = [
     {
         img: "project1.png",
         title: "مشروع 1",
-        description:"نبذة عن مشروع 1"
+        description:"نبذة عني"
 
     },
 
     {
         img: "project2.png",
         title: " مشروع 2",
-        description:"نبذة عن مشروع 2"
+        description:"TO-DO list"
+
+    },
+    {
+        img: "project3.png",
+        title: " مشروع 3",
+        description:"TO-DO list"
 
     },
 ]
